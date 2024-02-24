ed9f005d02880c34a2ddc9c31e5f488d6e696e43
Laura Silveira <laura.rra58@gmail.com>
2024-02-24 20:43:29 -0300

    Criando pedido das pizzas

--- process/pizza.php
+++ process/pizza.php
@@ -34,13 +34,55 @@
         //Validação de sabores máximos
         if(count($sabores) > 3){
 
-            $_SESSION["msg"] = "Selecione no máximo 3 sabores!";
+            $_SESSION["msg"] = "Selecione no máximo 3 sabores";
             $_SESSION["status"] = "warning";
 
         }else{
 
-            echo "passou da validação";
-            exit;
+            // salvando borda e massa na pizza
+            $stmt = $conn->prepare("INSERT INTO pizzas (borda_id, massa_id) VALUES (:borda, :massa)");
+
+
+            //filtrando inputs
+            $stmt->bindParam(":borda" , $borda , PDO::PARAM_INT);
+            $stmt->bindParam(":massa" , $massa , PDO::PARAM_INT);
+
+            $stmt->execute();
+
+            //resgatando o último id da última pizza
+            $pizzaId = $conn->lastInsertId();
+
+            $stmt = $conn->prepare("INSERT INTO pizza_sabor (pizza_id , sabor_id) VALUES (:pizza ,
+            :sabor)");
+
+            //repetição até salvar todos sabores
+            $stmt = $conn->prepare("INSERT INTO pizza_sabor (pizza_id , sabor_id) VALUES (:pizza , :sabor)");
+ 
+            foreach ($sabores as $sabor) {
+             
+                // filtrando os inputs
+                $stmt->bindParam(":pizza", $pizzaId, PDO::PARAM_INT);
+                $stmt->bindParam(":sabor", $sabor, PDO::PARAM_INT);
+             
+                $stmt->execute();
+            }
+
+            //Criar o pedido da pizza
+            $stmt = $conn->prepare("INSERT INTO pedidos(pizza_id , status_id) VALUES (:pizza , :status)");
+
+            //status-> sempre inicia com 1, que é em produção
+            $statusId = 1;
+
+            //filtrar inputs
+            $stmt->bindParam(":pizza", $pizzaId);
+            $stmt->bindParam(":status", $statusId);
+
+            $stmt->execute();
+
+            //Exibir mensagem de sucesso
+            $_SESSION["msg"] = "Pedido realizado com sucesso";
+            $_SESSION["status"] = "sucess";
+
 
         }
 
